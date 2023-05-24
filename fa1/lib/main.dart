994ae6a88ccad8f3cfced2f34697ee978b258db2
Rashidjon <tursunovrashidjon4@gmail.com>
2023-05-24 09:08:14 +0500

    ranglar va text o`zgartirildi

--- fa1/lib/main.dart
+++ fa1/lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bato sanagich',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Bato sanagich bosh sahifa'),
     );
   }
 }
